9b58f42e8ea154ae2d79c1170b12a7226751eb8e
juliangracia16-crypto <juliangracia16@outlook.com>
2026-04-08 11:04:45 -0700

    color en Form Registrar ingrediente

--- RestaurantePresentacion/src/main/java/com/mycompany/restaurantepresentacion/FrmRegistrarNuevoIngrediente.java
+++ RestaurantePresentacion/src/main/java/com/mycompany/restaurantepresentacion/FrmRegistrarNuevoIngrediente.java
@@ -19,6 +19,7 @@ public class FrmRegistrarNuevoIngrediente extends javax.swing.JFrame {
     
     /**
      * Creates new form FrmRegistrarNuevoIngrediente
+     * @param ingredientesBO
      */
     public FrmRegistrarNuevoIngrediente(IIngredientesBO ingredientesBO) {
         this.ingredientesBO = ingredientesBO;
@@ -37,7 +38,6 @@ public class FrmRegistrarNuevoIngrediente extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        lblTItulo = new javax.swing.JLabel();
         lblNombreIngrediente = new javax.swing.JLabel();
         txtNombreIngrediente = new javax.swing.JTextField();
         lblStockInicial = new javax.swing.JLabel();
@@ -45,32 +45,79 @@ public class FrmRegistrarNuevoIngrediente extends javax.swing.JFrame {
         lblUnidadMedida = new javax.swing.JLabel();
         comboUnidadMedida = new javax.swing.JComboBox<>();
         btnRegistrar = new javax.swing.JButton();
-        btnCancelar = new javax.swing.JButton();
+        btnVolver = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        lblTitulo2 = new javax.swing.JLabel();
+        lblTItulo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        lblTItulo.setText("Registro De Ingrediente");
-
+        lblNombreIngrediente.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        lblNombreIngrediente.setForeground(new java.awt.Color(255, 255, 255));
         lblNombreIngrediente.setText("Nombre del Ingrediente: ");
 
+        txtNombreIngrediente.setForeground(new java.awt.Color(255, 255, 255));
+
+        lblStockInicial.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        lblStockInicial.setForeground(new java.awt.Color(255, 255, 255));
         lblStockInicial.setText("Stock Inicial: ");
 
+        lblUnidadMedida.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        lblUnidadMedida.setForeground(new java.awt.Color(255, 255, 255));
         lblUnidadMedida.setText("Unidad de Medida: ");
 
+        btnRegistrar.setBackground(new java.awt.Color(0, 128, 0));
+        btnRegistrar.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btnRegistrar.setForeground(new java.awt.Color(255, 255, 255));
         btnRegistrar.setText("Registrar");
         btnRegistrar.addActionListener(this::btnRegistrarActionPerformed);
 
-        btnCancelar.setText("Cancelar");
-        btnCancelar.addActionListener(this::btnCancelarActionPerformed);
+        btnVolver.setBackground(new java.awt.Color(66, 66, 66));
+        btnVolver.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btnVolver.setForeground(new java.awt.Color(255, 255, 255));
+        btnVolver.setText("Volver");
+        btnVolver.addActionListener(this::btnVolverActionPerformed);
+
+        jPanel2.setBackground(new java.awt.Color(67, 82, 90));
+
+        lblTitulo2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lblTitulo2.setForeground(new java.awt.Color(255, 255, 255));
+        lblTitulo2.setText("Ingredientes — Nuevo Ingrediente");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(302, Short.MAX_VALUE)
+                .addComponent(lblTitulo2)
+                .addGap(283, 283, 283))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblTitulo2)
+                .addContainerGap(20, Short.MAX_VALUE))
+        );
+
+        lblTItulo.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lblTItulo.setForeground(new java.awt.Color(33, 84, 164));
+        lblTItulo.setText("Registro De Ingrediente");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(24, 24, 24)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(24, 24, 24)
+                        .addComponent(btnRegistrar, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnVolver, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())
+                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lblNombreIngrediente)
                             .addComponent(lblStockInicial))
@@ -81,24 +128,22 @@ public class FrmRegistrarNuevoIngrediente extends javax.swing.JFrame {
                         .addGap(52, 52, 52)
                         .addComponent(lblUnidadMedida)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(comboUnidadMedida, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(comboUnidadMedida, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(285, 285, 285)
-                        .addComponent(btnRegistrar, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(15, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(lblTItulo)
-                .addGap(303, 303, 303))
+                        .addGap(293, 293, 293)
+                        .addComponent(lblTItulo)))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(26, 26, 26)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lblTItulo)
-                .addGap(34, 34, 34)
+                .addGap(42, 42, 42)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblNombreIngrediente)
                     .addComponent(txtNombreIngrediente, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -111,15 +156,15 @@ public class FrmRegistrarNuevoIngrediente extends javax.swing.JFrame {
                 .addGap(98, 98, 98)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnRegistrar, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(42, Short.MAX_VALUE))
+                    .addComponent(btnVolver, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -133,10 +178,10 @@ public class FrmRegistrarNuevoIngrediente extends javax.swing.JFrame {
         this.registrarNuevoIngrediente();
     }//GEN-LAST:event_btnRegistrarActionPerformed
 
-    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
+    private void btnVolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVolverActionPerformed
         this.vaciarForm();
         this.dispose();
-    }//GEN-LAST:event_btnCancelarActionPerformed
+    }//GEN-LAST:event_btnVolverActionPerformed
     
     private void llenarComboUnidadMedida(){
         comboUnidadMedida.removeAllItems();
@@ -179,13 +224,15 @@ public class FrmRegistrarNuevoIngrediente extends javax.swing.JFrame {
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnCancelar;
     private javax.swing.JButton btnRegistrar;
+    private javax.swing.JButton btnVolver;
     private javax.swing.JComboBox<UnidadMedida> comboUnidadMedida;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JLabel lblNombreIngrediente;
     private javax.swing.JLabel lblStockInicial;
     private javax.swing.JLabel lblTItulo;
+    private javax.swing.JLabel lblTitulo2;
     private javax.swing.JLabel lblUnidadMedida;
     private javax.swing.JTextField txtNombreIngrediente;
     private javax.swing.JTextField txtStockInicial;
